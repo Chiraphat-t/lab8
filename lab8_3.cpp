a5760ddb8289862244735578298cb0dfc6bf059f
Chiraphat-t <chiraphat.ttt@gmail.com>
2024-12-06 16:03:27 +0700

    donee

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,h,b;
+    string name;
+    cout << "Enter your age: ";
+    cin >> a;
+
+    if(a<=25){
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<100){
+            name="Chopper";
+        }
+        else if(h<180){
+            name ="Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if(b<=1100000000){
+                name = "Sanji";
+            }
+            else{
+                name = "Zoro";
+            }
+        }
+    }
+    else if(a<=60 && a>25){
+        cout << "Enter your bounty: ";
+            cin >> b;
+            if(b<=500000000){
+                name = "Franky";
+            }
+            else{
+                name = "Jinbe";
+            }
+    }
+    else{
+        name = "Brook";
+    }
+cout << "Your character = "<< name;
+
+}
